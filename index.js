81e7c6e6fc1e8c720e79c415fe0a7f8370e6c0d8
Naajdorf <138386839+Naajdorf@users.noreply.github.com>
2023-07-10 11:48:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+const config = {
+  discordToken: 'MTEyMjExNDM0NDY4NDExNDA2MQ.Ge8BVI.z8C135Ogg5LrOee3Q85FOMLnZIAcp6Wtsc7Los'
+};
+
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+  console.log(`[ViralBot] Pomyślnie zalogowałeś się do systemu.`);
+
+  const activity = ':green_circle: Graczy: 1';
+
+  client.user.setActivity(activity);
+  console.log(`Aktywność ustawiona na: ${activity}`);
+});
+
+client.login(config.discordToken);
